6ef95166e4333a128d66cfa82d2878f5e0ba645e
tr1789 <t.renard1789@gmail.com>
2026-04-25 21:56:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.get("/analyze", async (req, res) => {
   const url = req.query.url;
 
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url, {
+  headers: {
+    "User-Agent": "Mozilla/5.0"
+  }
+});
+
     const $ = cheerio.load(data);
 
     const title = $("title").text();
